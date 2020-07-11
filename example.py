18918e7d53ff14db6b20904d8b7af13657175595
Dmitry Bevz <d.p.bevz@gmail.com>
2020-07-11 20:40:17 +0300

    Create example.py

---
+++ example.py
@@ -0,0 +1,22 @@
+from Unicorn import load_markedup_profession  # Выгрузка размеченных данных
+
+
+# Берем навыки для Менеджера по продажам    
+skills_raw =  load_markedup_profession('Менеджер по продажам')
+skills_list = skills_raw.index.drop_duplicates().to_list()
+
+# Создаем парсер на все выбранные скилы
+skills_list = skills_raw.index.drop_duplicates().to_list()
+skills_objects = []
+for skill in skills_list:
+    # В цикле собираем все объекты навыков
+    skill_keywords = make_keywords_dict(skills_raw.loc[skill, 'keywords'])
+    skill_kind = skills_raw.loc[skill, 'kind']
+    skill_kind = skill_kind if type(skill_kind)==str else skill_kind.unique()
+    
+    skills_objects.append(
+        Skill(name=skill, kind=skill_kind, keywords=skill_keywords)
+    )
+    
+
+
