1af2a5aa759af0d77a2de895cb053048c97cc91c
Dmitry Bevz <d.p.bevz@gmail.com>
2020-07-12 11:15:11 +0300

    Update example.py

--- example.py
+++ example.py
@@ -1,15 +1,17 @@
-from Unicorn import load_markedup_profession, make_keywords_dict, Skill  # Выгрузка размеченных данных
+from Unicorn import load_markedup_profession, make_keywords_dict, Skill, Position  # Выгрузка размеченных данных
+from Unicorn import PRELOAD_PROFESSIONS
 
-
-# Берем навыки для Менеджера по продажам    
-skills_raw =  load_markedup_profession('Продавец')
+# Берем размеченные навыки для Менеджера по продажам
+# Для удобства = они хранятся в датафрейме
+skills_raw =  load_markedup_profession(PRELOAD_PROFESSIONS[0])
 skills_list = skills_raw.index.drop_duplicates().to_list()
 
-# Создаем парсер на все выбранные скилы
+# Создаем объекты навыкина все выбранные навыки
+# Объекты будут определять факт наличия у кандидата навыка
 skills_list = skills_raw.index.drop_duplicates().to_list()
 skills_objects = []
 for skill in skills_list:
-    # В цикле собираем все объекты навыков
+    # В цикле собираем все объекты навыков в одну группу
     skill_keywords = make_keywords_dict(skills_raw.loc[skill, 'keywords'])
     skill_kind = skills_raw.loc[skill, 'kind']
     skill_kind = skill_kind if type(skill_kind)==str else skill_kind.unique()
@@ -17,4 +19,9 @@ for skill in skills_list:
     skills_objects.append(
         Skill(name=skill, kind=skill_kind, keywords=skill_keywords)
     )
+    
+# Далее все объекты навыков можно привязать к объекту позиции
+# Создание объекта "менеджер по продажам"
+sales_manager = Position(name=PRELOAD_PROFESSIONS[0], experience='between3And6', skills=skills_objects)
 
+# Данный объект парсит из текста 
