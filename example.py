6d6d847265680e17e45839742f2584a771e64b2f
Dmitry Bevz <d.p.bevz@gmail.com>
2020-07-12 12:04:59 +0300

    Update example.py

--- example.py
+++ example.py
@@ -25,3 +25,24 @@ for skill in skills_list:
 sales_manager = Position(name=PRELOAD_PROFESSIONS[0], experience='between3And6', skills=skills_objects)
 
 # Данный объект парсит из текста 
+# Например возьмем датасет из примера к заданию
+resumes = pd.read_csv('resume_train.csv', delimiter=';', encoding='utf-8')
+
+# Отфильтруем только нужную позицию
+def filter_by_position(data, position_pattern):
+    """Функция фильтрует по названию профессии и возвращает индексы совпавших с паттерном строк
+    data - это массив данных
+    position_pattern - это паттерн позиции, который мы ищем, объект Re"""
+    result = data.str.lower().str.contains(pattern)
+    return result
+    
+    
+# Сохраняем только сейлз менеджеров
+pattern = re.compile('(менеджер по продажам|sales manager|сейлз-менеджер)')  # Задаются три группы схожести названия должности
+resumes_sales_managers = resumes.dropna().copy()
+resumes_sales_managers = resumes_sales_managers.loc[filter_by_position(resumes.position, pattern)]
+
+
+# Из отфильтрованных резюме парсим данные о скиллах
+result = resumes_sales_managers.head(10).description.map(sales_manager.check_skills).apply(pd.Series)
+result
