5ac88e53e47b235f7db99a1bd87cb247027d7c5f
Dmitry Bevz <d.p.bevz@gmail.com>
2020-07-12 11:01:49 +0300

    Поправил check_skills
    Нельзя по умолчанию переменную назначать на объект self

--- Unicorn.py
+++ Unicorn.py
@@ -147,13 +147,17 @@ class Position:
             'moreThan6': (lambda x: x > 6)
         }[self.experience]
     
-    def check_skills(self, text=None, key_words=self.last_text_key_words):
+    def check_skills(self, text=None):
         """Функция проверяет наличие навыков в текстовом описание кандидата, либо в списке ключевых слов. 
         По умолчанию использует последний проанализированный текст, если была проверка до этого"""
-        if text!=None:
-            key_words=self.get_list_skills(text)
         if text==None and key_words==None:
             raise TypeError
+            
+        if text!=None:
+            key_words=self.get_list_skills(text)
+        else:
+            key_words=self.last_text_key_words
+
         result = {}
         for skill in self.skills:
             result[skill.name] = skill.find_skill(key_words)
